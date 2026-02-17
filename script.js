7cea86c083f5fce8d6fb3deab1c3de720e916510
karianjahi <joseph@spiced-academy.com>
2026-02-17 16:06:33 +0100

    Refactor the script to have single functions that are short and concise to search and highlight matches

--- script.js
+++ script.js
@@ -5,48 +5,37 @@ const testResult = document.getElementById("result");
 const caseInsensitiveFlag = document.getElementById("i");
 const globalFlag = document.getElementById("g");
 
+
 const getFlags = () => {
-    if (caseInsensitiveFlag.checked && globalFlag.checked) return "gi";
-    if (caseInsensitiveFlag.checked && !globalFlag.checked) return "i";
-    if (!caseInsensitiveFlag.checked && !globalFlag.checked) return "";
-    if (!caseInsensitiveFlag.checked && globalFlag.checked) return "g";
+     if (caseInsensitiveFlag.checked && globalFlag.checked) return "gi";
+     if (caseInsensitiveFlag.checked && !globalFlag.checked) return "i";
+     if (!caseInsensitiveFlag.checked && globalFlag.checked) return "g";
+     if (!caseInsensitiveFlag.checked && !globalFlag.checked) return "";
 };
 
-const getUniqueItems = (array) => {
+
+const createValidRegex = (pattern, flag) => {
+    return new RegExp(pattern, flag);
+};
+
+const getMatches = (text, pattern) => {
+    const flag = getFlags();
+    // const flag = "gi";
+    const regex = createValidRegex(pattern,flag);
+    console.log(regex);
+    return text.match(regex);
+};
+
+const getUniqueArray = (array) => {
     return [...new Set(array)];
 };
 
-
-
-const matchRegex = (text, pattern, flags) => {
-    if (pattern === "\d+") pattern = "\\d+";
-    const regex = new RegExp(pattern, flags);
-    const resultArray = getUniqueItems(text.match(regex));
-    if (resultArray.length === 0) return "no match";
-    if (flags.includes("g")) {
-        for (let item of resultArray) {
-           text = text.replaceAll(item, `<span class="highlight">${item}</span>`);
-        }
-        console.log(text);
-        return text
-    } 
-    console.log(text.replace(resultArray[0], `<span class="highlight">${resultArray[0]}</span>`) );
-    return text.replace(resultArray[0], `<span class="highlight">${resultArray[0]}</span>`);
+const makeReplacementOfMatches = (text, pattern) => {
+    const uniqueMatches = getUniqueArray(getMatches(text, pattern));
+    for (let item of uniqueMatches)
+        text = text.replace(item, `<span class="highlight">${item}</span>`)
+    return text
 };
 
-// console.log(matchRegex("Gu1n34 P1g5", "\d+", "gi"))
-
-// console.log(matchRegex("I am awake 798 hasemiCha", "\d+", "gi"));
-
-regexPattern.addEventListener("input", ()=> {
-    return regexPattern.value;
-});
-
-testButton.addEventListener("click", ()=> {
-    const regex = regexPattern.value;
-    const testString = stringToTest.textContent;
-    testResult.innerHTML =  matchRegex(testString, regex, getFlags());
-});
-
-
+// console.log(makeReplacementOfMatches("I have no dog in the fight since the DOG I had was stolen in the year 1994. Good thing is that i might still bring a new doG bought in year 2014. The new dog will be mofire though the other Dog was not as it was about 16 years old", "[0-9]+"));
 
